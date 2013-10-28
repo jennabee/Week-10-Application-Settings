a4563461c6419f55b68792f8bcc801a989259936
jennabee <webstudent@PC16302.local>
2013-10-28 17:48:05 -0500

    changed colors

--- Week 10 ApplicationSettings/ApplicationSettings/ApplicationSettingsViewController.m
+++ Week 10 ApplicationSettings/ApplicationSettings/ApplicationSettingsViewController.m
@@ -23,9 +23,9 @@ NSString *favoriteColorSelected;
 - (void)viewDidLoad {
     //---create an array containing the colors values---
     colors = [[NSMutableArray alloc] init];
-    [colors addObject:@"Red"];
-    [colors addObject:@"Green"];
-    [colors addObject:@"Blue"];
+    [colors addObject:@"Yellow"];
+    [colors addObject:@"Purple"];
+    [colors addObject:@"Orange"];
     [super viewDidLoad];
 }
 
